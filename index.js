8f1079db5f70169631a08b26a10168a17938a0f7
AntoineBap <129936013+AntoineBap@users.noreply.github.com>
2023-11-27 18:50:18 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,76 @@
+document.addEventListener('DOMContentLoaded', function(){
+    let playerText = document.getElementById('playerText');
+    let restartBtn = document.getElementById('restartBtn');
+    let boxes = Array.from(document.getElementsByClassName('box'));
+
+
+    const X_TEXT = "X";
+    const O_TEXT = "O";
+    let currentPlayer = X_TEXT;
+    let spaces = Array(9).fill(null);   
+    
+    const startGame = () => {
+        boxes.forEach(box => box.addEventListener('click', boxClicked));
+    }
+
+    function boxClicked(e) {
+        const id = e.target.id;
+        if(!spaces[id]){
+            spaces[id] = currentPlayer
+            e.target.innerText = currentPlayer
+            currentPlayer = currentPlayer == X_TEXT ? O_TEXT : X_TEXT;
+            console.log(spaces);
+
+            if(playerHasWon() !== false){
+                let winningBlocks = playerHasWon()
+                document.querySelector('h1').innerHTML = `${spaces[winningBlocks[0]]} wins !`
+                console.log(winningBlocks);
+            };
+
+            if(playerHasWon() == false){
+                let noPossibleMove = spaces.every(element => element !== null)
+                if(noPossibleMove){
+                    document.querySelector('h1').innerHTML = 'Draw...'
+                }else{
+                    return;
+                }
+            }
+        }
+    }
+
+const winningCombos = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+]
+
+function playerHasWon() {
+    for(condition of winningCombos) {
+        let [a,b,c] = condition
+
+        if(spaces[a] !== null &&
+            spaces[a] == spaces[b]&&
+            spaces[b] == spaces[c]){
+                return[a,b,c]
+            }
+    }
+    return false
+}
+
+    restartBtn.addEventListener('click', restart)
+    function restart(){
+        spaces.fill(null)
+        boxes.forEach(box => {
+            box.innerText = ""
+        })
+        document.querySelector('h1').innerHTML='Tic Tac Toe'
+        currentPlayer = X_TEXT
+    }
+    startGame()
+})
+
